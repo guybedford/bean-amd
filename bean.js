af48d12693c7f50726a4493b779e078e2607f706
guybedford <guybedford@gmail.com>
2013-02-25 17:53:58 +0200

    update for amdquery changes

--- bean.js
+++ bean.js
@@ -19,13 +19,11 @@ define(['amdquery', 'bean-lib'], function($, bean) {
   var beanApply = function(name, evt) {
     var fn = bean[name];
     return function() {
-      var arr = arguments[0];
-
       if (evt)
-        Array.prototype.splice.call(arguments, 1, 0, evt);
+        Array.prototype.splice.call(arguments, 0, 0, evt);
 
-      for (var i = 0, len = arr.length; i < len; i++) {
-        arguments[0] = arr[i];
+      for (var i = 0, len = this.length; i < len; i++) {
+        Array.prototype.unshift.call(arguments, this[i]);
         fn.apply(this, arguments);
       }
       // allow chaining
@@ -36,12 +34,12 @@ define(['amdquery', 'bean-lib'], function($, bean) {
   // core methods
   var methods = ['on', 'one', 'off', 'clone', 'fire'];
   for (var i = 0; i < methods.length; i++)
-    methodObj[methods[i]] = beanApply(methods[i]);
+    methodObj.prototype[methods[i]] = beanApply(methods[i]);
 
   // events to create shortcuts for
   var events = ['blur', 'change', 'click', 'dblclick', 'error', 'focus', 'focusin', 'focusout', 'keydown', 'keypress', 'keyup', 'load', 'mousedown', 'mouseenter', 'mouseleave', 'mouseout', 'mouseover', 'mouseup', 'mousemove', 'resize', 'scroll', 'select', 'submit', 'unload'];
   for (var i = 0; i < events.length; i++)
-    methodObj[events[i]] = beanApply('on', events[i]);
+    methodObj.prototype[events[i]] = beanApply('on', events[i]);
 
   bean.setSelectorEngine($);
 
